42e9f3808b86a0152c75c3b72a9a527082e5ec69
Jasper V <jasper.verbon@gmail.com>
2026-04-22 21:13:43 +0200

    [types] Add additional type signatures

--- hello.hs
+++ hello.hs
@@ -1,26 +1,48 @@
 main :: IO ()
 main = putStrLn myHtml
 
+-- HTML printing
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+--  Tags (document)
+
 html_ :: String -> String
-html_ content = "<!DOCTYPE html><html>" <> content <> "</html>"
+html_ = el "html"
 
 head_ :: String -> String
-head_ content = "<head>" <> content <> "</head>"
+head_ = el "head"
 
 title_ :: String -> String
-title_ content = "<title>" <> content <> "</title>"
+title_ = el "title"
 
 body_ :: String -> String
-body_ content = "<body>" <> content <> "</body>"
+body_ = el "body"
+
+-- Tags (content)
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
 
 -- makeHtml
 makeHtml :: String -> String -> String
 makeHtml titleContent bodyContent =
-  html_
-    ( head_
-        (title_ titleContent)
-        <> body_ bodyContent
-    )
+  "<!DOCTYPE html>"
+    <> html_
+      ( head_
+          (title_ titleContent)
+      )
+    <> body_ bodyContent
 
 myHtml :: String
-myHtml = makeHtml "My page title" "My page content"
+myHtml =
+  makeHtml
+    "My page title"
+    ( h1_ "My page content"
+        <> p_ "My first paragraph"
+    )
